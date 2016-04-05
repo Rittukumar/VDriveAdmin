82653e94faffa73b7478df7cc600b004027774d8
Selva <selvasajin@gmail.com>
2016-04-05 13:09:12 +0530

    Api url change discarded
    api url changed from local to production

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -1795,11 +1795,11 @@ evezownApp.constant('AUTH_EVENTS', {
 });
 
 evezownApp.constant('PATHS', {
-    api_url: 'http://localhost:8000/v1/'
+    //api_url: 'http://localhost:8000/v1/'
     //api_url: 'http://creativethoughts.co.in/evezown/api/public/v1/'
     //api_url: 'http://evezown.com/api/public/v1/'
     //api_url: 'http://evezown.com/beta/api/public/v1/'
-    //api_url: 'http://evezown-api-dev.elasticbeanstalk.com/public/v1/'
+    api_url: 'http://evezown-api-dev.elasticbeanstalk.com/public/v1/'
 });
 
 evezownApp.constant('USER_ROLES', {
