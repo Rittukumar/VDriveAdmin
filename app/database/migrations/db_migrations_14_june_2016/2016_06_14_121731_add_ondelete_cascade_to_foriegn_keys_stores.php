a17b0c218c7ade57680525dc3ec326738c699401
vasanth-evezown <vasanth@evezown.com>
2016-06-15 10:24:51 +0530

    migration fixes

--- app/database/migrations/db_migrations_14_june_2016/2016_06_14_121731_add_ondelete_cascade_to_foriegn_keys_stores.php
+++ app/database/migrations/db_migrations_14_june_2016/2016_06_14_121731_add_ondelete_cascade_to_foriegn_keys_stores.php
@@ -14,7 +14,6 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 	{
 		Schema::table('store_front_images', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_front_images_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
@@ -22,7 +21,6 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('store_owners', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_owners_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
@@ -30,7 +28,6 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('store_business_info', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_business_info_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
@@ -38,20 +35,21 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('store_subscription_offer', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_subscription_offer_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
+				  ->onDelete('cascade');
+
+			$table->foreign('subscription_id')
+				  ->references('id')->on('store_subscription_offer')
 				  ->onDelete('cascade');
 		});
 
 		Schema::table('store_tags', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_tags_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
 
-			$table->dropForeign('store_tags_tag_id_foreign');
 			$table->foreign('tag_id')
 				  ->references('id')->on('tags')
 				  ->onDelete('cascade');
@@ -59,12 +57,10 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('trending_products', function(Blueprint $table)
 		{
-		    $table->dropForeign('trending_products_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
 
-			$table->dropForeign('trending_products_product_sku_id_foreign');
 			$table->foreign('product_sku_id')
 				  ->references('id')->on('product_sku')
 				  ->onDelete('cascade');
@@ -72,12 +68,10 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('store_front_info', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_front_info_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
 
-			$table->dropForeign('store_front_info_listing_type_id_foreign');
 			$table->foreign('listing_type_id')
 				  ->references('id')->on('store_listing_type')
 				  ->onDelete('cascade');
@@ -85,7 +79,6 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('store_commerce', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_commerce_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
@@ -93,7 +86,6 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('store_front_promotions', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_front_promotions_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
@@ -101,23 +93,40 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('store_front_promotion_images', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_front_promotion_images_store_promotion_id_foreign');
 			$table->foreign('store_promotion_id')
 				  ->references('id')->on('store_front_promotions')
 				  ->onDelete('cascade');
+
+			$table->foreign('promotion_image1_id')
+				  ->references('id')->on('images')
+				  ->onDelete('cascade');
+
+			$table->foreign('promotion_image2_id')
+			      ->references('id')->on('images')
+			      ->onDelete('cascade');
+
+			$table->foreign('promotion_image3_id')
+			      ->references('id')->on('images')
+			      ->onDelete('cascade');
+
+			$table->foreign('promotion_image4_id')
+			      ->references('id')->on('images')
+			      ->onDelete('cascade');
 		});
 
 		Schema::table('store_advertising', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_advertising_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
+
+			 $table->foreign('recco_subscription_id')
+				   ->references('id')->on('recco_subscriptions')
+				   ->onDelete('cascade');
 		});
 
 		Schema::table('store_status', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_status_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
@@ -130,35 +139,35 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 
 		Schema::table('store_front_comments', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_front_comments_store_id_foreign');
+			$table->dropForeign('store_front_comments_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
 
-			$table->dropForeign('store_front_comments_user_id_foreign');
+            $table->dropForeign('store_front_comments_user_id_foreign');
 			$table->foreign('user_id')
 				  ->references('id')->on('users')
 				  ->onDelete('cascade');
 
-			$table->dropForeign('store_front_comments_comment_id_foreign');
+            $table->dropForeign('store_front_comments_comment_id_foreign');
 			$table->foreign('comment_id')
 				  ->references('id')->on('comments')
 				  ->onDelete('cascade');
 		});
-
+			
 		Schema::table('store_front_grades', function(Blueprint $table)
 		{
-		    $table->dropForeign('store_front_grades_store_id_foreign');
+			$table->dropForeign('store_front_grades_store_id_foreign');
 			$table->foreign('store_id')
 				  ->references('id')->on('stores')
 				  ->onDelete('cascade');
 
-			$table->dropForeign('store_front_grades_grader_id_foreign');
+            $table->dropForeign('store_front_grades_grader_id_foreign');
 			$table->foreign('grader_id')
 				  ->references('id')->on('users')
 				  ->onDelete('cascade');
 
-			$table->dropForeign('store_front_grades_grade_id_foreign');
+            $table->dropForeign('store_front_grades_grade_id_foreign');
 			$table->foreign('grade_id')
 				  ->references('id')->on('grades')
 				  ->onDelete('cascade');
@@ -193,6 +202,7 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 		Schema::table('store_subscription_offer', function(Blueprint $table)
 		{
 			$table->dropForeign('store_subscription_offer_store_id_foreign');
+			$table->dropForeign('store_subscription_offer_subscription_id_foreign');
 
 		});
 
@@ -232,19 +242,24 @@ class AddOndeleteCascadeToForiegnKeysStores extends Migration {
 		Schema::table('store_front_promotion_images', function(Blueprint $table)
 		{
 			$table->dropForeign('store_front_promotion_images_store_promotion_id_foreign');
+			$table->dropForeign('store_front_promotion_images_promotion_image1_id_foreign');
+			$table->dropForeign('store_front_promotion_images_promotion_image2_id_foreign');
+			$table->dropForeign('store_front_promotion_images_promotion_image3_id_foreign');
+			$table->dropForeign('store_front_promotion_images_promotion_image4_id_foreign');
 
 		});
 
 		Schema::table('store_advertising', function(Blueprint $table)
 		{
 			$table->dropForeign('store_advertising_store_id_foreign');
+			$table->dropForeign('store_advertising_recco_subscription_id_foreign');
 
 		});
 
 		Schema::table('store_status', function(Blueprint $table)
 		{
 			$table->dropForeign('store_status_store_id_foreign');
-			DB::statement("ALTER TABLE store_status DROP FOREIGN KEY fk_status_id");
+			$table->dropForeign('store_status_status_id_foreign');
             
 		});
 
